ff07dc4366444b3502d1f76fe779409b39544f97
Abdul <abdul.a@addendanalytics.com>
2023-01-03 17:05:49 +0530

    UI Uploaded

--- app.py
+++ app.py
@@ -1,7 +1,20 @@
 from datetime import datetime
 from flask import Flask, render_template, request, redirect, url_for, send_from_directory
-app = Flask(__name__)
+from azure.cosmos import CosmosClient, PartitionKey
+from flask import *;
+from datetime import datetime
 
+endpoint = "https://dp420abdul.documents.azure.com:443/"
+key = "S63SFJSI8GMQYbPxO21W9vL4N2D94nFr3yJwyO3ZAf8yhfzefU7XSd7RQ82c1abS5wkTEYz1O0vDACDb8Jp9sg=="
+
+client = CosmosClient(url=endpoint, credential=key)
+
+
+database = client.get_database_client('journalapp')
+container = database.get_container_client('user_details')
+
+
+app = Flask(__name__)
 
 @app.route('/')
 def index():
@@ -23,6 +36,27 @@ def hello():
    else:
        print('Request for hello page received with no name or blank name -- redirecting')
        return redirect(url_for('index'))
+
+
+@app.route('/success',methods = ['POST', 'GET'])  
+def success():  
+   if request.method == 'POST':
+      fname = request.form.get('Fname')
+      dept = request.form.get('dept')
+      notes = request.form.get('notes')
+      mood = request.form.get('mood')
+      userID = str(datetime.now()) + fname
+      newItem = {
+        "id": str(datetime.now()),
+        "userId": userID,
+        "Name":fname,
+        "dept": dept,
+        "mood": mood,
+        "notes": notes
+        }
+      container.create_item(newItem)
+      return redirect(url_for('index'))
+
 
 
 if __name__ == '__main__':
