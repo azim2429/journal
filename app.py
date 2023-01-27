4c7d7f4e1270e7fa9057d7567f55c7bc034e2784
Abdul <abdul.a@addendanalytics.com>
2023-01-27 18:11:27 +0530

    Changed Input Fields

--- app.py
+++ app.py
@@ -1,10 +1,8 @@
 from datetime import datetime
 from azure.cosmos import CosmosClient, PartitionKey
-from flask import *;
+from flask import *
 import requests
 import simplejson as json
-
-
 
 
 endpoint = "https://dp420abdul.documents.azure.com:443/"
@@ -24,104 +22,110 @@ container = database.get_container_client('user_details')
 
 app = Flask(__name__)
 
+
 @app.route('/')
 def index():
-   print('Request for index page received')
-   value = list(container.read_all_items(max_item_count=10))
-   return render_template('index.html',value=value,len=len(value))
+   value = list(container.read_all_items(max_item_count=50))
+   return render_template('index.html', value=value, len=len(value))
 
-@app.route('/delete',methods = ['POST', 'GET'])  
-def delete():  
+
+@app.route('/delete', methods=['POST', 'GET'])
+def delete():
    if request.method == 'POST':
     id = request.form.get('delete')
     userId = request.form.get('userId')
-    container.delete_item(id,partition_key=userId)
+    container.delete_item(id, partition_key=userId)
     return redirect(url_for('index'))
 
-@app.route("/upsert",methods = ['POST', 'GET'])
+
+@app.route("/upsert", methods=['POST', 'GET'])
 def upsert():
     if request.method == 'POST':
         id = request.form.get('id')
         userId = request.form.get('userId')
-        item = (container.read_item(id,partition_key=userId))
+        item = (container.read_item(id, partition_key=userId))
         item['Name'] = request.form.get('Fname')
         item['dept'] = request.form.get('dept')
         item['notes'] = request.form.get('notes')
-        item['last_updated'] = str(datetime.now().strftime('%Y/%m/%d %I:%M:%S'))
+        item['last_updated'] = str(
+            datetime.now().strftime('%Y/%m/%d %I:%M:%S'))
 
-        querystring = {"text":item['notes']}
-        response = requests.request("GET", url, headers=headers, params=querystring)
+        querystring = {"text": item['notes']}
+        response = requests.request(
+            "GET", url, headers=headers, params=querystring)
         var = (json.loads(response.text))
 
-        if(var['sentiment']=='NEUTRAL'):
+        if (var['sentiment'] == 'NEUTRAL'):
           item['mood'] = 'https://img.icons8.com/emoji/512/neutral-face.png'
-      
-        if(var['sentiment']=='POSITIVE'):
+
+        if (var['sentiment'] == 'POSITIVE'):
           item['mood'] = 'https://img.icons8.com/emoji/512/star-struck.png'
 
-        if(var['sentiment']=='NEGATIVE'):
+        if (var['sentiment'] == 'NEGATIVE'):
           item['mood'] = 'https://img.icons8.com/emoji/512/weary-face.png'
 
-        if(var['sentiment']=='WEAK_NEGATIVE'):
+        if (var['sentiment'] == 'WEAK_NEGATIVE'):
           item['mood'] = 'https://img.icons8.com/emoji/512/pensive-face.png'
-      
-        if(var['sentiment']=='WEAK_POSITIVE'):
+
+        if (var['sentiment'] == 'WEAK_POSITIVE'):
           item['mood'] = 'https://img.icons8.com/emoji/512/smiling-face-with-smiling-eyes.png'
 
         container.upsert_item(body=item)
         return redirect(url_for('index'))
 
-@app.route('/update',methods = ['POST', 'GET'])  
-def update():  
+
+@app.route('/update', methods=['POST', 'GET'])
+def update():
    if request.method == 'POST':
     id = request.form.get('id')
     userId = request.form.get('userId')
-    item = (container.read_item(id,partition_key=userId))
-    return render_template('update.html',item=item)
+    item = (container.read_item(id, partition_key=userId))
+    return render_template('update.html', item=item)
 
-@app.route('/success',methods = ['POST', 'GET'])  
-def success():  
+
+@app.route('/success', methods=['POST', 'GET'])
+def success():
    if request.method == 'POST':
       fname = request.form.get('Fname')
       dept = request.form.get('dept')
       notes = request.form.get('notes')
-      userID = str(datetime.now()) + fname
 
-      querystring = {"text":notes}
-      response = requests.request("GET", url, headers=headers, params=querystring)
+      querystring = {"text": notes}
+      response = requests.request(
+          "GET", url, headers=headers, params=querystring)
       var = (json.loads(response.text))
 
-      if(dept == 'Select your Department'):
+      if (dept == 'Select your Department'):
          dept = 'Other'
 
-      if(var['sentiment']=='NEUTRAL'):
+      if (var['sentiment'] == 'NEUTRAL'):
         mood = 'https://img.icons8.com/emoji/512/neutral-face.png'
-      
-      if(var['sentiment']=='POSITIVE'):
+
+      if (var['sentiment'] == 'POSITIVE'):
         mood = 'https://img.icons8.com/emoji/512/star-struck.png'
 
-      if(var['sentiment']=='NEGATIVE'):
+      if (var['sentiment'] == 'NEGATIVE'):
         mood = 'https://img.icons8.com/emoji/512/weary-face.png'
 
-      if(var['sentiment']=='WEAK_NEGATIVE'):
+      if (var['sentiment'] == 'WEAK_NEGATIVE'):
         mood = 'https://img.icons8.com/emoji/512/pensive-face.png'
-      
-      if(var['sentiment']=='WEAK_POSITIVE'):
+
+      if (var['sentiment'] == 'WEAK_POSITIVE'):
         mood = 'https://img.icons8.com/emoji/512/smiling-face-with-smiling-eyes.png'
 
-      
       newItem = {
-        "id": str(datetime.now()),
-        "userId": userID,
-        "Name":fname,
-        "dept": dept,
-        "notes": notes,
-        "mood":mood,
-        "mood_res":var['sentiment'],
-        "last_updated":str(datetime.now().strftime('%Y/%m/%d %I:%M:%S'))
-        }
+          "id": str(datetime.now()),
+          "userId": fname,
+          "Name": fname,
+          "dept": dept,
+          "notes": notes,
+          "mood": mood,
+          "mood_res": var['sentiment'],
+          "last_updated": str(datetime.now().strftime('%Y/%m/%d %I:%M:%S'))
+      }
       container.create_item(newItem)
       return redirect(url_for('index'))
+
 
 if __name__ == '__main__':
    app.run()
